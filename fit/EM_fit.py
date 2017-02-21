fixing calculation error

fit/EM_fit.py
@@ -4,6 +4,7 @@ from fit import E_step
 from fit import M_step
 
 def EM_fit(model, data, tol = None, maxiter = None):
+
     if tol is None: tol = 1e-3
     if maxiter is None: maxiter = 100
 
@@ -17,20 +18,21 @@ def EM_fit(model, data, tol = None, maxiter = None):
     init_softcounts = np.zeros((2, 1))
     log_likelihoods = np.zeros((maxiter, 1))
 
-    # print(data)
-    # print(model)
-    # print(trans_softcounts)
-    # print(emission_softcounts)
-    # print(init_softcounts)
-    # result = E_step.run(data, model, trans_softcounts, emission_softcounts, init_softcounts, 1)
+    result = {}
+    result['all_trans_softcounts'] = trans_softcounts
+    result['all_emission_softcounts'] = emission_softcounts
+    result['all_initial_softcounts'] = init_softcounts
 
     for i in range(maxiter):
-        result = E_step.run(data, model, trans_softcounts, emission_softcounts, init_softcounts, 1)
-        log_likelihoods[i] = result['total_loglike']
+        result = E_step.run(data, model, result['all_trans_softcounts'], result['all_emission_softcounts'], result['all_initial_softcounts'], 1)
+        for j in range(num_resources):
+            result['all_trans_softcounts'][j] = result['all_trans_softcounts'][j].transpose()
 
-        if(i > 1 and abs(log_likelihoods[i] - log_likelihoods[i-1]) < tol):
+        log_likelihoods[i][0] = result['total_loglike']
+
+        if(i > 1 and abs(log_likelihoods[i][0] - log_likelihoods[i-1][0]) < tol):
             break
 
-        model = M_step.run(model, trans_softcounts, emission_softcounts, init_softcounts)
+        model = M_step.run(model, result['all_trans_softcounts'], result['all_emission_softcounts'], result['all_initial_softcounts'])
 
-    return(model, log_likelihoods[1:i])
+    return(model, log_likelihoods[:i+1])

generate/synthetic_data.py
@@ -3,27 +3,37 @@ from generate import synthetic_data_helper
 
 #TODO: check that parameters are not null, have data, match sizes, etc.
 def synthetic_data(model, lengths, resources = None):
-    num_resources = model["learns"].shape[0]
-    bigT = sum(lengths)
 
-    if resources is None: resources = np.random.randint(1, high = num_resources+1, size = bigT) #used to be 1xbigT
-    if "As" not in model: model["As"] = np.array([[1-model["learns"][0], model["forgets"][0]], [model["learns"], 1-model["forgets"]]]) #TODO: is not considering more that one resource for this. fix it.
+    num_resources = len(model["learns"])
+    bigT = sum(lengths) #is it ok to fix the first dim to 0?
 
-    if "pi_0" not in model: model["pi_0"] = np.array([1-model["prior"], model["prior"]])
+    if resources is None: resources = np.random.randint(1, high = num_resources+1, size = bigT)
+    if "As" not in model: model["As"] = np.array([[1-model["learns"], model["forgets"]], [model["learns"], 1-model["forgets"]]])
 
-    #col array from 1 to 49901 on steps of 100
-    starts = np.cumsum(lengths)
+    if "pi_0" not in model: model["pi_0"] = np.array([[1-model["prior"]], [model["prior"]]])
+
+    starts = np.cumsum(lengths) #is it ok to fix the first dim to 0?
     starts = np.array([starts[i] - lengths[i] + 1 for i in range(len(starts))])
 
-    #print(model)
-    #print(starts)
-    #print(lengths)
-    #print(resources)
     syn_data = synthetic_data_helper.create_synthetic_data(model, starts, lengths, resources)
-    #print(syn_data["data"][0])
     syn_data["data"] = syn_data["data"] + 1
 
     syn_data["data"][:, resources != 1] = 0 #no data emitted unless resource == 1
+
+    #for i in range(bigT):
+    #    if i%2 == 0:
+    #        resources[i] = 1
+    #        syn_data["stateseqs"][0][i] = 0
+    #    else:
+    #        resources[i] = 2
+    #        syn_data["stateseqs"][0][i] = 1
+    #    if i%3 == 0:
+    #        syn_data["data"][0][i] = 0
+    #    elif i%3 == 1:
+    #        syn_data["data"][0][i] = 1
+    #    else:
+    #        syn_data["data"][0][i] = 2
+
 
     datastruct = {}
     datastruct["stateseqs"] = syn_data["stateseqs"]