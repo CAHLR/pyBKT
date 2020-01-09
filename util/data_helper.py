ec55644219b7c39eb86b493c59874b4d585dc52c
rachelcusack <49796187+rachelcusack@users.noreply.github.com>
2020-01-09 14:50:18 -0800

    updated data_helper.py
    Put in active URL for dataset and modified some code to properly read the csv.

--- util/data_helper.py
+++ util/data_helper.py
@@ -4,30 +4,35 @@ def assistments_data(skill_name):
   import numpy as np
   import io
   import requests
-
-  url = "http://users.wpi.edu/~yutaowang/data/skill_builder_data.csv"
-  s = requests.get(url).content
-  df = pd.read_csv(io.StringIO(s.decode('ISO-8859-1')))
   
+  print(skill_name)
+  url = "https://drive.google.com/uc?export=download&id=0B3f_gAH-MpBmUmNJQ3RycGpJM0k"
+  s = requests.get(url).content
+  # df = pd.read_csv(io.BytesIO(s))
+  df = pd.read_csv(io.StringIO(s.decode('latin')))
+  # df = pd.read_csv(io.StringIO(s.decode('latin')), names = ['order_id', 'assignment_id', 'user_id', 'assistment_id', 'problem_id', 'original', 'correct', 'attempt_count', 'ms_first_response', 'tutor_mode', 'answer_type', 'sequence_id', 'student_class_id', 'position', 'type', 'base_sequence_id', 'skill_id', 'skill_name', 'teacher_id', 'school_id', 'hint_count', 'hint_total', 'overlap_time', 'template_id', 'answer_id', 'answer_text', 'first_action', 'bottom_hint', 'opportunity', 'opportunity_original'])
   # filter by the skill you want, make sure the question is an 'original'
-  skill = df[(df['skill_name']==skill_name) & (df['original'] == 1)]
+  # skills = df["skill_name"]
+  # for skill_name in skills:
+  skill = df[(df["skill_name"]==skill_name) & (df["original"] == 1)]
   # sort by the order in which the problems were answered
-  df.sort_values('order_id', inplace=True)
-
+  df["order_id"] = [int(i) for i in df["order_id"]]
+  df.sort_values("order_id", inplace=True)
+  
   # example of how to get the unique users
   # uilist=skill['user_id'].unique()
 
   # convert from 0=incorrect,1=correct to 1=incorrect,2=correct
-  skill.loc[:,'correct']+=1
+  skill.loc[:,"correct"]+=1
   
   # filter out garbage
-  df3=skill[skill['correct']!=3]
-  data=df3['correct'].values
+  df3=skill[skill["correct"]!=3]
+  data=df3["correct"].values
   
   # find out how many problems per user, form the start/length arrays
-  steps=df3.groupby('user_id')['problem_id'].count().values
+  steps=df3.groupby("user_id")["problem_id"].count().values
   lengths=np.copy(steps)
-  
+  # print("steps", steps)
   steps[0]=0
   steps[1]=1
   for i in range(2,steps.size):
@@ -48,3 +53,4 @@ def assistments_data(skill_name):
   Data["resources"]=resource
   
   return (Data)
+
