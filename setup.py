1f3c522cd888a0afbc2b24c145c34ceb66e4e08a
Anirudhan Badrinath <abadrinath@berkeley.edu>
2020-04-08 18:44:07 -0700

    added manifest

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ try:
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8'
+            'Programming Language :: Python :: 3.8',
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
@@ -160,7 +160,7 @@ except:
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8'
+            'Programming Language :: Python :: 3.8',
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
