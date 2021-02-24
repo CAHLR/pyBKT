181d9672382e650a3e1daf52bee28dfca6fca7f4
Anirudhan Badrinath <abadrinath@berkeley.edu>
2021-02-23 16:22:15 -0800

    update to version 1.2.1

--- setup.py
+++ setup.py
@@ -172,7 +172,7 @@ try:
 
     setup(
         name="pyBKT",
-        version="1.2",
+        version="1.2.1",
         author="Zachary Pardos, Anirudhan Badrinath, Matthew Jade Johnson, Christian Garay",
         author_email="zp@berkeley.edu, abadrinath@berkeley.edu, mattjj@csail.mit.edu, c.garay@berkeley.edu",
         license = 'MIT',
@@ -202,7 +202,7 @@ try:
 except:
     setup(
         name="pyBKT",
-        version="1.2",
+        version="1.2.1",
         author="Zachary Pardos, Anirudhan Badrinath, Matthew Jade Johnson, Christian Garay",
         author_email="zp@berkeley.edu, abadrinath@berkeley.edu, mattjj@csail.mit.edu, c.garay@berkeley.edu",
         license = 'MIT',
