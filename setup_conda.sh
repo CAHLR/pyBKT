dc66973df17500f9ef28f0f27ca1ddf106189c0c
abadrinath947 <abadrinath@berkeley.edu>
2021-04-05 14:31:09 -0700

    fixed conda install

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -e -p "Please enter the location of the Conda install (no relative paths like .. or ~; absolute path only); tab to complete: " conda_path
+conda_path=$(conda info --envs | grep base | awk 'NF>1{print $NF}')
 conda env create -f environment.yml
 export BOOST_INCLUDE="$conda_path/envs/pyBKT/include" && export LD_LIBRARY_PATH="$conda_path/envs/pyBKT/lib" && conda activate pyBKT && pip install pyBKT
 echo "Done installing pyBKT!"
