dd6cd830fbb5cced7b819d4f9099a53cc2709ec7
Anirudhan Badrinath <abadrinath@berkeley.edu>
2021-10-09 12:22:39 -0700

    removed conda install

--- setup_conda.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-conda_path=$(conda info --envs | grep base | awk 'NF>1{print $NF}')
-conda env create -f environment.yml
-export BOOST_INCLUDE="$conda_path/envs/pyBKT/include" && export LD_LIBRARY_PATH="$conda_path/envs/pyBKT/lib" && conda activate pyBKT && pip install pyBKT
-echo "Done installing pyBKT!"
